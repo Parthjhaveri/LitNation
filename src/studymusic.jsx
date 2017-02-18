14e027472e22e1057f46461cd6db3aaa3ed65f0e
Parthjhaveri <parthjhaveri209@gmail.com>
2017-02-18 16:24:23 -0500

    latest

--- src/studymusic.jsx
+++ src/studymusic.jsx
@@ -8,17 +8,27 @@ import { IndexRoute, Link, Router, Route, browserHistory } from 'react-router';
 var StudyMusic = React.createClass({
   render:function() {
     return (
-      <div className="studymusicdiv">
-      {
-        // IFRAME FOR STUDY MUSIC LINK:
-        // http://streaming64.radionomy.com/JamendoLounge?lang=en-US%2cen%3bq%3d0.8%2cnb%3bq%3d0.6%2cnl%3bq%3d0.4
-      }
-        <center>
-          <p id="studyheading">STUDYING AND CODING MUSIC</p>
-          <iframe src="http://streaming64.radionomy.com/JamendoLounge?lang=en-US%2cen%3bq%3d0.8%2cnb%3bq%3d0.6%2cnl%3bq%3d0.4" className="radioframe">
-          </iframe>
-          <p id="studyheadinglive">Live stream...</p>
-        </center>
+      <div>
+        <div className="studymusicdiv">
+        {
+          // IFRAME FOR STUDY MUSIC LINK:
+          // http://streaming64.radionomy.com/JamendoLounge?lang=en-US%2cen%3bq%3d0.8%2cnb%3bq%3d0.6%2cnl%3bq%3d0.4
+        }
+          <center>
+            <p id="studyheading">STUDYING AND CODING MUSIC</p>
+            <iframe src="http://streaming64.radionomy.com/JamendoLounge?lang=en-US%2cen%3bq%3d0.8%2cnb%3bq%3d0.6%2cnl%3bq%3d0.4" className="radioframe">
+            </iframe>
+          </center>
+        </div>
+        {
+          // STATS DIV
+        }
+        <div className="playerstats">
+          <div className="framewrapper">
+            <iframe src="https://rmo.shoutcast.com/Stats/Live" id="statsbox">
+            </iframe>
+          </div>
+        </div>
       </div>
     )
   }
